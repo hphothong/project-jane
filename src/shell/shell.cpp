4e3fb1c302741126316336ffbcf864dac553d28a
Hayden Phothong <hphothong@gmail.com>
2021-09-19 18:04:53 -0600

    Update shell to call lexer

--- src/shell/shell.cpp
+++ src/shell/shell.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <string>
+#include "../compiler/lexer.hpp"
 
 using namespace std;
 
 int main() {
   string input;
   while (true) {
-    cout << "shell>";
-    getline(cin, input);
-    cout << input << endl;
+    try {
+      cout << "shell>";
+      getline(cin, input);
+      Lexer lexer = Lexer("shell", input);
+      vector<Token> tokens = lexer.getTokens();
+      for (auto token : tokens) {
+        cout << token.toString() << endl;
+      }
+    } catch (runtime_error const& e) {
+      cout << e.what() << endl;
+    }
   }
 }
